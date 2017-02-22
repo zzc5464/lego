9eac5d925af1a2dcf92cfa9083bf458bf2008d73
王伟琪 <wangweiqi@wangweiqi-mbp.local>
2017-02-22 18:51:33 +0800

    增加图标组件的示例

--- src/basic/icon/icon.vue
+++ src/basic/icon/icon.vue
@@ -1,14 +1,23 @@
 <template>
-    <i class='iconfont' :class='[iconName]'><slot></slot></i>
+    <i class='iconfont' :class='[iconName]' :style='styleObj'></i>
 </template>
 
 <script>
     module.exports = {
-        props: [ 'name' ],
-
+        props: [ 'name', 'size', 'color' ],
         data: function () {
+            var colors = {
+                'black' : '#333',
+                'grey'  : '#666',
+                'light' : '#999'
+            };
+
             return {
-                iconName: 'icon-' + this.name
+                iconName: 'icon-' + this.name,
+                styleObj: {
+                    fontSize: this.size + 'rem',
+                    color: colors[this.color] || '#666'
+                }
             };
         }
     }
