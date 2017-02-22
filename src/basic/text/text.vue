df3fd4c617341ba34fea67e0e4306d9a400b2bdd
xuanzhuan <1418213168@qq.com>
2017-02-22 11:26:27 +0800

    scss

--- src/basic/text/text.vue
+++ src/basic/text/text.vue
@@ -1,3 +1,18 @@
 <template>
     <span><slot></slot></span>
 </template>
+<script>
+    module.exports = {
+       
+    }
+</script>
+
+<style scoped lang="scss">
+@import '../../sass/lego.scss';
+
+
+    span {
+        font-size: 16px;
+        color: #666;
+    }
+</style>
