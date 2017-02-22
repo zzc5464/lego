d21bf84663ee8cbbb748b87468e38126cdadf7bd
王伟琪 <wangweiqi@wangweiqi-mbp.local>
2017-02-22 15:48:34 +0800

    加下划线

--- src/structure/cell/cell.vue
+++ src/structure/cell/cell.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class='_cell' :style='styleObj'>
+    <div class='_cell' :class='classObj' :style='styleObj'>
         <slot></slot>
     </div>
 </template>
 
 <script>
     module.exports = {
-        props: [ 'height' ],
+        props: [ 'height', 'border' ],
         data: function() {
             return {
                 styleObj: {
                     height: this.height + 'rem'
+                },
+                classObj: {
+                    '_border': this.border === 'true'
                 }
             };
         }
--- src/structure/single-cell/single-cell.vue
+++ src/structure/single-cell/single-cell.vue
@@ -1,3 +1,19 @@
 <template>
-    <div class='_single-cell'><slot></slot></div>
+    <div class='_single-cell' :class='classObj' :style='styleObj'><slot></slot></div>
 </template>
+
+<script>
+    module.exports = {
+        props: [ 'height', 'border' ],
+        data: function() {
+            return {
+                styleObj: {
+                    height: this.height + 'rem'
+                },
+                classObj: {
+                    '_border': this.border === 'true'
+                }
+            };
+        }
+    }
+</script>
