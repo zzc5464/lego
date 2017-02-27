c8d8dd2b11b9fd0d0ff47005db5f969de9c80106
xuanzhuan <1418213168@qq.com>
2017-02-27 11:20:34 +0800

    style change basic

--- src/basic/highlight/highlight.vue
+++ src/basic/highlight/highlight.vue
@@ -1,5 +1,6 @@
 <template>
-    <span :style="obj"><slot></slot></span>
+    <!--<span :style="obj"><slot></slot></span>-->
+    <span class="_highlight" :class="[ textColor, fontSize ]"><slot></slot></span>
 </template>
 
 <script>
@@ -7,23 +8,26 @@
         props: [ 'size', 'color' ],
 
         data: function() {
-            var colors = {
-                'brown'  : '#b38f47',
-                'red' : '#d6654c'
-            };
+            var obj = {};
 
-            return {
-                obj: {
-                    fontSize: this.size + 'rem',
-                    color: colors[this.color] || '#d7ac62'
-                }
-            };
+            this.color && (obj.textColor = '_text_color_' + this.color);
+            this.size && (obj.fontSize = '_text_size_' + this.size + 'px');
+
+            return obj;
         }
+
+        // data: function() {
+        //     var colors = {
+        //         'brown'  : '#b38f47',
+        //         'red' : '#d6654c'
+        //     };
+
+        //     return {
+        //         obj: {
+        //             fontSize: this.size + 'rem',
+        //             color: colors[this.color] || '#d7ac62'
+        //         }
+        //     };
+        // }
     }
 </script>
-
-<style scoped>
-span {
-    font-weight: normal;
-}
-</style>
--- src/basic/icon/icon.vue
+++ src/basic/icon/icon.vue
@@ -1,29 +1,39 @@
 <template>
-    <i class='pingan' :class='[iconName]' :style='styleObj'></i>
+    <i class='pingan' :class='[ iconName, textColor, fontSize ]' ></i>
 </template>
 
 <script>
     module.exports = {
         props: [ 'name', 'size', 'color' ],
-        data: function () {
-            // var colors = {
-            //     'black' : '#333',
-            //     'grey'  : '#666',
-            //     'light' : '#999'
-            // };
 
-            return {
-                iconName: 'i-' + this.name,
-                styleObj: {
-                    fontSize: this.size + 'rem',
-                    color: this.color
-                }
-            };
+        data: function() {
+            var obj = {};
+
+            this.name && (obj.iconName = 'i-' + this.name);
+            this.color && (obj.textColor = '_text_color_' + this.color);
+            this.size && (obj.fontSize = '_text_size_' + this.size + 'px');
+
+            return obj;
         }
+        // data: function () {
+        //     var colors = {
+        //         'black' : '#333',
+        //         'grey'  : '#666',
+        //         'light' : '#999'
+        //     };
+
+        //     return {
+        //         iconName: 'i-' + this.name,
+        //         styleObj: {
+        //             fontSize: this.size + 'rem',
+        //             color: colors[this.color]
+        //         }
+        //     };
+        // }
     }
 </script>
-<style scoped>
+<!--<style scoped>
     i {
         color: #666;
     }
-</style>
+</style>-->
