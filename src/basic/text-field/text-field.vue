fda4fb888228cab2540f3b7383998a4c54224b6f
王伟琪 <wangweiqi@wangweiqi-mbp.local>
2017-02-28 16:43:15 +0800

    Improve <b-text-field/> and <b-number-field/>

--- src/basic/text-field/text-field.vue
+++ src/basic/text-field/text-field.vue
@@ -1,13 +1,19 @@
 <template>
-    <input class="_input" :class="classObj" type="text" name="" :placeholder="placeholder" maxlength="max" :required="required" >
+    <input class='_text_field' :class='classObj' :style='styleObj' type='text' name='name' :placeholder='placeholder' :maxlength='max' :required='required' >
 </template>
 
 <script>
     module.exports = {
-        props: ['size', 'max', 'placeholder', 'required'],
+        props: [ 'name', 'size', 'align', 'max', 'placeholder', 'required' ],
+
         data: function() {
             var obj = {}, list = [];
-            this.size && list.push('_text_size_' + this.size + 'px');
+
+            this.align 
+            && (obj.textAlign = this.align);
+
+            this.size 
+            && list.push('_text_size_' + this.size + 'px');
 
             return {
                 classObj: list,
