2ef562e9c1a1781c7e85fc7db4d65c0de9ec8719
王伟琪 <wangweiqi@wangweiqi-mbp.local>
2017-03-07 11:27:27 +0800

    Add page background

--- src/page/main/main.vue
+++ src/page/main/main.vue
@@ -1,5 +1,27 @@
 <template>
-    <main class='_main'>
-        <slot></slot>
-    </main>
+    <div class='_main_wrp' :class='classObj'>
+        <main class='_main'>
+            <slot></slot>
+        </main>
+    </div>
 </template>
+
+<script>
+    module.exports = {
+        props: [ 
+            'bgcolor'
+        ],
+
+        data: function () {
+            var list = [];
+
+            this.bgcolor && list.push(
+                '_bgcolor_' + this.bgcolor
+            );
+
+            return {
+                classObj: list
+            }
+        }
+    }
+</script>
