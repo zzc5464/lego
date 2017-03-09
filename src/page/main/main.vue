ab210b1ed3b9219fde75474286d11b1dce7510ec
王伟琪 <wangweiqi@wangweiqi-mbp.local>
2017-03-09 17:56:04 +0800

    Add cell group tag.

--- src/page/main/main.vue
+++ src/page/main/main.vue
@@ -1,7 +1,9 @@
 <template>
     <div class='_main_wrp' :class='classObj'>
         <main class='_main'>
-            <slot></slot>
+            <div class='_cell_group' v-for='slotNames in slotNames' :class='slotNames.className'>
+                <slot :name='slotNames.name'></slot>
+            </div>
         </main>
     </div>
 </template>
@@ -19,8 +21,49 @@
                 '_bgcolor_' + this.bgcolor
             );
 
+            var slots    = this.$slots.default,
+                group    = null,
+                groups   = [],
+                pre      = null,
+                bgcolor  = null,
+                props;
+
+            slots.forEach(function (vnode) {
+                if (typeof vnode.componentOptions !== 'undefined') {
+                    props = vnode.componentOptions.propsData;
+                    bgcolor = props.bgcolor || '';
+                    props.bgcolor = '';
+
+                    if (pre === null || pre !== bgcolor) {
+                        group = {
+                            bgcolor: bgcolor, 
+                            slots: []
+                        };
+
+                        groups.push(group);
+                    }
+
+                    pre = bgcolor;
+                    group.slots.push(vnode);
+                }
+            });
+
+            var slotNames = [], name;
+
+            for (var i = 0, l = groups.length; i < l; i++) {
+                name = 'slot-' + i;
+                bgcolor = groups[i].bgcolor;
+                this.$slots[name] = groups[i].slots;
+
+                slotNames.push({
+                    name        : name, 
+                    className   : bgcolor ? '_bgcolor_' + bgcolor : ''
+                });
+            }
+
             return {
-                classObj: list
+                classObj: list,
+                slotNames: slotNames
             }
         }
     }
