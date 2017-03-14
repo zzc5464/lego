8547fdadf52fda955307b96e72d7b93117e7d4ce
xuanzhuan <1418213168@qq.com>
2017-03-14 18:31:28 +0800

    toast

--- src/page/toast/toast.vue
+++ src/page/toast/toast.vue
@@ -1,7 +1,7 @@
 <template>
     <aside class="_toast">
-        <div>
-            {{ message }}
+        <div class="_toast_container">
+            <slot></slot>
         </div>
     </aside>
 </template>
