e41880eb76e660fd9caa65825405351aa7cd3bb4
王伟琪 <wangweiqi@wangweiqi-mbp-2.local>
2017-03-20 09:12:26 +0800

    暂时去掉自动增加 div 的功能

--- src/page/main/main.vue
+++ src/page/main/main.vue
@@ -2,9 +2,10 @@
     <div class='_main_wrp_toast'>
         <div class='_main_wrp' :class='classObj'>
             <main class='_main'>
-                <div class='_cell_group' v-for='slotNames in slotNames' :class='slotNames.className'>
+                <slot></slot>
+                <!--<div class='_cell_group' v-for='slotNames in slotNames' :class='slotNames.className'>
                     <slot :name='slotNames.name'></slot>
-                </div>
+                </div>-->
             </main>
         </div>
         <div class='_toast' :class='toastClass'>
@@ -37,49 +38,49 @@
                 '_bgcolor_' + this.bgcolor
             );
 
-            var slots    = this.$slots.default,
-                group    = null,
-                groups   = [],
-                pre      = null,
-                bgcolor  = null,
-                props;
+            // var slots    = this.$slots.default,
+            //     group    = null,
+            //     groups   = [],
+            //     pre      = null,
+            //     bgcolor  = null,
+            //     props;
 
-            slots.forEach(function (vnode) {
-                if (typeof vnode.componentOptions !== 'undefined') {
-                    props = vnode.componentOptions.propsData;
-                    bgcolor = props.bgcolor || '';
-                    props.bgcolor = '';
+            // slots.forEach(function (vnode) {
+            //     if (typeof vnode.componentOptions !== 'undefined') {
+            //         props = vnode.componentOptions.propsData;
+            //         bgcolor = props.bgcolor || '';
+            //         props.bgcolor = '';
 
-                    if (pre === null || pre !== bgcolor) {
-                        group = {
-                            bgcolor: bgcolor, 
-                            slots: []
-                        };
+            //         if (pre === null || pre !== bgcolor) {
+            //             group = {
+            //                 bgcolor: bgcolor, 
+            //                 slots: []
+            //             };
 
-                        groups.push(group);
-                    }
+            //             groups.push(group);
+            //         }
 
-                    pre = bgcolor;
-                    group.slots.push(vnode);
-                }
-            });
+            //         pre = bgcolor;
+            //         group.slots.push(vnode);
+            //     }
+            // });
 
-            var slotNames = [], name;
+            // var slotNames = [], name;
 
-            for (var i = 0, l = groups.length; i < l; i++) {
-                name = 'slot-' + i;
-                bgcolor = groups[i].bgcolor;
-                this.$slots[name] = groups[i].slots;
+            // for (var i = 0, l = groups.length; i < l; i++) {
+            //     name = 'slot-' + i;
+            //     bgcolor = groups[i].bgcolor;
+            //     this.$slots[name] = groups[i].slots;
 
-                slotNames.push({
-                    name        : name, 
-                    className   : bgcolor ? '_bgcolor_' + bgcolor : ''
-                });
-            }
+            //     slotNames.push({
+            //         name        : name, 
+            //         className   : bgcolor ? '_bgcolor_' + bgcolor : ''
+            //     });
+            // }
 
             return {
                 classObj: list,
-                slotNames: slotNames,
+                //slotNames: slotNames,
 
                 toastMsg: '',
                 toastOpened: false,
