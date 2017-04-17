7198a39a11233a3e03211fa626c66e69e3cef5fa
sipingqiqi <me@wangweiqi.com>
2017-04-17 15:12:46 +0800

    timeline

--- src/assemble/timeline/timeline.vue
+++ src/assemble/timeline/timeline.vue
@@ -1,32 +1,28 @@
 <template>
-    <div class="_timeline" v-html='creatSvg'>
-    </div>
+    <div class="_timeline" v-html='creatSvg'></div>
 </template>
 
 <script>
     module.exports = {
         props: [ 'length', 'elements'],
         data: function() {
-            // var ele = [
-            //     {
-            //         time: '2016-04-19',
-            //         desc: '提交申购'
-            //     },
-            //     {
-            //         time: '2016-04-20',
-            //         desc: '审核成功'
-            //     },
-            //     {
-            //         time: '2016-04-21',
-            //         desc: '等待结果'
-            //     },
-            //     {
-            //         time: '2016-04-22',
-            //         desc: '等待收益'
-            //     }
-            // ];
-            var ele = this.elements;
-            var len = ele.length;
+
+            var slots = this.$slots.default,
+                elems = [], options, children;
+
+            slots.forEach(function (s) {
+                options = s.componentOptions;
+                if (options && options.propsData) {
+                    elems.push({
+                        label : options.propsData.label,
+                        desc  : options.propsData.desc
+                    });
+                }
+            });
+
+
+            var ele = elems;
+            var len = elems.length;
             var x = len == '3' ? '-47' : '-4';
 
             var temp = '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="'+ x +',0,375,95">\
@@ -34,23 +30,7 @@
                         <rect fill="#d7ac62" x="15%" y="30" width="21%" height="0.5"/>';
             var cx = ["15%","36%","59%","81%"];
             var rx = ["15%","38%","61%"];
-            var tx = [
-                {
-                    x1: "6%",
-                    x2: "9%"
-                },
-                {
-                    x1: "26.5%",
-                    x2: "29.5%"
-                },
-                {
-                    x1: "49.8%",
-                    x2: "52.8%"
-                },
-                {
-                    x1: "71.5%",
-                    x2: "74.5%"
-                }];
+
             //圆点
             for (var i = 1; i < (len); i++) {
                 temp += '<circle stroke="#ffffff" stroke-width="3" fill="#c1c1c1" cx="'+ cx[i]+'" cy="30" r="6"/>';
@@ -61,8 +41,8 @@
             }
             //文字
             for (var i = 0; i < (len); i++) {
-                temp += '<text x="'+tx[i].x1+'" y="58" font-size="12" fill="#999">'+ ele[i].time +'</text>\
-                        <text x="'+tx[i].x2+'" y="75" font-size="12" fill="#999">'+ ele[i].desc +'</text>';
+                temp += '<text x="'+cx[i]+'" y="58" text-anchor="middle" font-size="12" fill="#999">'+ ele[i].label +'</text>\
+                        <text x="'+cx[i]+'" y="75" text-anchor="middle" font-size="12" fill="#999">'+ ele[i].desc +'</text>';
             }
 
             temp += '</svg>';
--- src/assemble/timepoint/timepoint.vue
+++ src/assemble/timepoint/timepoint.vue
@@ -1,20 +1,12 @@
 <template>
-    <!--<div :style='styleObj' class='_timepoint' :class='{_active: isActive}'>
-        <slot name='header' ></slot>
-        <slot name='datatime' ></slot>
-    </div>-->
+    <!-- 虚节点 -->
 </template>
 
 <script>
     module.exports = {
-        props: ['id', 'width', 'active'],
+        props: ['label', 'desc'],
         data: function() {
-            var obj = {};
-            this.width && (obj.width = this.width);
-            return {
-                isActive: this.active,
-                styleObj: obj
-            };
+            return { };
         }
     }
 </script>
