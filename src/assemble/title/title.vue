c3a9144ea28ad6b3a6fcb5a1cb6eb3e185ca2b6e
sipingqiqi <me@wangweiqi.com>
2017-05-04 17:34:22 +0800

    Fix bugs

--- src/assemble/title/title.vue
+++ src/assemble/title/title.vue
@@ -1,22 +1,50 @@
 <template>
     <s-cell height='2.263158' border='true' >
         <s-flex-column></s-flex-column>
-        <s-column width='12.315789' align='left'>
-            <b-text size='34' color='black'>{{ label }}</b-text>
+        <s-column :width='width.label' align='left'>
+            <b-text size='34' color='black'>{{ label }}</b-text>&nbsp;
             <b-text size='20' color='light'>{{ subtitle }}</b-text>
         </s-column>
-        <s-column width='5.578945' align='right'>
-            <b-text size='34' color='black'>{{ comment }}</b-text>
+        <s-column :width='width.value' align='right'>
+            <b-text v-if='comment !== ""' size='34' color='black'>{{ comment }}</b-text>
+            <b-text v-else size='34' color='high'><slot></slot></b-text>
         </s-column>
         <s-flex-column></s-flex-column>
     </s-cell>
 </template>
 
 <script>
+    var contants =  require('../../utils/contants');
+
     module.exports = {
-        props: [ 'label', 'subtitle', 'comment' ],
+        props: {
+            valueWidth: {
+                type: Number,
+                default: 5.578945
+            },
+
+            label: {
+                type: String,
+                default: ''
+            },
+
+            subtitle: {
+                type: String,
+                default: ''
+            },
+
+            comment: {
+                type: String,
+                default: ''
+            }
+        }, 
         data: function() {
-            return {};
+            return {
+                width: {
+                    label: contants.tableWidth - this.valueWidth,
+                    value: this.valueWidth
+                }
+            };
         }
     }
 </script>
