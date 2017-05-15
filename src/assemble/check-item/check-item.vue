76fa9156ca2bb79c9277254c4d7d3f1fb7a37936
wyq <wyq@wyqdeMacBook-Pro.local>
2017-05-15 14:02:24 +0800

    新增钱包，银行卡组件

---
+++ src/assemble/check-item/check-item.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- 虚节点 -->
+</template>
+
+<script>
+    module.exports = {
+        props: ['id', 'type', 'bankNum', 'checked'],
+        data: function() {
+            return {};
+        }
+    }
+</script>
