0413740fd0ec0752b5438d5c9386a6e632757339
xuanzhuan <1418213168@qq.com>
2017-05-25 19:11:24 +0800

    modified lead end

--- src/assemble/lead/lead.vue
+++ src/assemble/lead/lead.vue
@@ -44,12 +44,12 @@
     </div>
     <div v-else-if='type === 5' class="_assetheader" :class="classObj" :style="styleObj">
         <header>
-            <h6 class="_eyecenter">{{datas.header}} <b-icon size="40" name="eye" @tapped="hiddenData"></b-icon></h6>
+            <h6 class="_eyecenter">{{tempData.header}} <b-icon size="40" name="eye" @tapped="hiddenData" v-show="open == true"></b-icon><b-icon size="40" name="eye-closed" @tapped="hiddenData" v-show="open != true" ></b-icon></h6>
             <h2>{{tempData.invest}}</h2>
         </header>
         <ul class="_three _second">
             <li>
-                <div>{{ datas.title1 }}</div>
+                <div>{{ tempData.title1 }}</div>
                 <div>{{ tempData.value1 }}</div>
             </li>
             <li>
@@ -86,7 +86,7 @@
 
             label: {
                 type: String,
-                default: '1'
+                default: ''
             },
 
             topData: {
@@ -96,40 +96,44 @@
         },
         data: function() {
             var obj = {}, list = [], t = 0;
+            var temp = {};
+            for(var i in this.topData) {
+                temp[i] = this.topData[i];
+            }
 
-            if (this.header === '' && this.footer === '') {
+            if (this.label === '1') {
+                t = 4;
+            } 
+            else if (this.label === '2') {
+                t = 5;
+            } 
+            else if (this.header === '' && this.footer === '') {
                 obj.height = '3.684211rem';
                 list.push('_setflex');
                 t = 0;
             }
 
-            if (this.header !== '' && this.footer === '') {
+            else if (this.header !== '' && this.footer === '') {
                 obj.paddingTop = '1.052632rem';
                 t = 1;
             }
 
-            if (this.header === '' && this.footer !== '') {
+            else if (this.header === '' && this.footer !== '') {
                 t = 0;
             }
 
-            if (this.header !== '' && this.footer !== '' && this.link === '') {
+            else if (this.header !== '' && this.footer !== '' && this.link === '') {
                 t = 2;
             }
 
-            if (this.header !== '' && this.footer !== '' && this.link !== '') {
+            else if (this.header !== '' && this.footer !== '' && this.link !== '') {
                 t = 3;
             }
-
-            if (this.label === '1') {
-                t = 4;
-            }
-
-            if (this.label === '2') {
-                t = 5;
-            }
+            
             return {
                 tempData: this.topData,
-                datas: this.topData,
+                datas: temp,
+                open: true,
                 classObj: list,
                 styleObj: obj,
                 type: t
@@ -137,22 +141,17 @@
         },
         methods: {
             hiddenData: function() {
-                // var tempData = {};    
-                var invest, value1, value2 ;
-                if(this.datas.invest !== '****') {
-                    invest = this.datas.invest;
-                    value1 = this.datas.value1;
-                    value2 = this.datas.value2;
+ 
+                if(this.tempData.invest !== '****') {
                     this.tempData.invest = '****';
                     this.tempData.value1 = '****';
                     this.tempData.value2 = '****';
+                    this.open = false;
                 } else {
-                    this.tempData.invest = invest;
-                    this.tempData.value1 = value1;
-                    this.tempData.value2 = value2;
-                    // this.datas.invest = tempData.invest;
-                    // this.datas.value1 = tempData.value1;
-                    // this.datas.value2 = tempData.value2;
+                    this.tempData.invest = this.datas.invest;
+                    this.tempData.value1 = this.datas.value1;
+                    this.tempData.value2 = this.datas.value2;
+                    this.open = true;
                 }
                 
             }
