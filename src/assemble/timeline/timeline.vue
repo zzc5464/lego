07001387e319c410b3500bb598fd6c7ed82312e2
xuanzhuan <1418213168@qq.com>
2017-05-26 10:09:02 +0800

    modified timeline

--- src/assemble/timeline/timeline.vue
+++ src/assemble/timeline/timeline.vue
@@ -50,14 +50,25 @@
             var cy = ['30','90','150','210'];
             var ty = ['35','95','155','215'];
 
+            var tempH = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-4,0,375,95">\
+                        <circle stroke="#C1C1C1" stroke-width="4" fill="#C1C1C1" cx="13%" cy="30" r="4"/>\
+                        <rect fill="#C1C1C1" x="13%" y="30" width="35%" height="0.5"/>\
+                        <text x="13%" y="58" text-anchor="middle" font-size="12" fill="#999">'+ ele[0].label +'</text>\
+                        <text x="13%" y="75" text-anchor="middle" font-size="12" fill="#999">'+ ele[0].desc +'</text>';
+            var ch = ["13%","50%","85%","96%"];
+            var rh = ["13%","51%","81%"];
+
+
             //圆点
             for (var i = 1; i < (len); i++) {
                 temp += '<circle stroke="#ffffff" stroke-width="3" fill="#c1c1c1" cx="'+ cx[i]+'" cy="30" r="6"/>';
                 tempY += '<circle stroke="#ffffff" stroke-width="3" fill="#c1c1c1" cx="2%" cy="'+ cy[i] +'" r="6"/>';
+                tempH += '<circle stroke="#c1c1c1" stroke-width="1" fill="#c1c1c1" cx="'+ ch[i]+'" cy="30" r="6"/>';
             }
             //横线
             for (var i = 1; i < (len-1); i++) {
                 temp += '<rect fill="#c1c1c1" x="'+rx[i]+'" y="30" width="21%" height="0.5"/>';
+                tempH += '<rect fill="#c1c1c1" x="'+rh[i]+'" y="30" width="35%" height="0.5"/>';
             }
             //文字
             for (var i = 1; i < (len); i++) {
@@ -66,12 +77,27 @@
 
                 tempY += '<text x="6%" y="'+ ty[i] +'" font-size="15" fill="#999">'+ ele[i].label +'</text>\
                 <text x="26%" y="'+ ty[i] +'" font-size="11" fill="#999">'+ ele[i].desc +'</text>';
+
+                tempH += '<text x="'+ch[i]+'" y="58" text-anchor="middle" font-size="12" fill="#999">'+ ele[i].label +'</text>\
+                        <text x="'+ch[i]+'" y="75" text-anchor="middle" font-size="12" fill="#999">'+ ele[i].desc +'</text>';
             }
 
             temp += '</svg>';
             tempY +='</svg>';
+            var svgHtml = '';
+            switch(this.lineType)
+                {
+                case '2':
+                    svgHtml = tempY;
+                break;
+                case '3':
+                    svgHtml = tempH;
+                break;
+                default:
+                    svgHtml = temp;
+                }
             return {
-                creatSvg: this.lineType == '1' ? temp : tempY
+                creatSvg: svgHtml
             };
         },
         methods: {
